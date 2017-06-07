e0fb91ad5f881e41ecd825d9a32d34193df84028
szaske <github@zaske.com>
2017-06-07 01:23:08 -0700

    update unit tests to match design specs

--- test/tests.js
+++ test/tests.js
@@ -2,14 +2,8 @@ QUnit.test("isLeapYear Basics", function(assert) {
   function LeapTest(year, expected) {
     assert.equal(isLeapYear(year), expected);
   }
-  // is the year divisible by 4 test
-  LeapTest(2012, true);
-  LeapTest(4, true);
-
-  // is the year divisible by 400 test
-  LeapTest(400, true);
-  LeapTest(800, true);
-
-  //
-
+  LeapTest(2004, true); // is the year divisible by 4?
+  LeapTest(2000, true); // is the year divisible by 400?
+  LeapTest(1900, false); //A year is not a leap year if it is divisible by 100
+  LeapTest(1993, false); //not divisible by 4
 });
